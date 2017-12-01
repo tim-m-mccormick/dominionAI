f3b4eaab4b760f79426be7026909f1e07631690f
Tim McCormick <tim.mccormick.osu@gmail.com>
2017-12-01 11:42:55 -0500

    Card.py
    Added victory_card()

--- Card.py
+++ Card.py
@@ -7,9 +7,13 @@ Created on Fri Dec  1 10:50:40 2017
 class Card:
     
     def __init__(self, card='Copper'):
+        #default points is zero
+        self.points = 0
         #Check cards and call appropriate constructor
         if card in ['Copper', 'Silver', 'Gold', 'Platinum']:
             self.money_card(card)
+        elif card in ['Estate', 'Dutchy', 'Province', 'Colony']:
+            self.victory_card(card)
 
     def money_card(card):
         #Check what type of card
@@ -27,3 +31,20 @@ class Card:
             self.value = 5
         else:
             print('money_card() called using non-money arguement')
+            
+    def victory_card(card):
+        #check what type of victory card
+        if card == 'Estate':
+            self.cost = 2
+            self.points = 1
+        elif card == 'Dutchy':
+            self.cost = 5
+            self.points = 3
+        elif card == 'Province':
+            self.cost = 8
+            self.points = 6
+        elif card == 'Colony':
+            self.cost = 11
+            self.points = 10
+        else:
+            print('victory_card() called with non-victory arguement')

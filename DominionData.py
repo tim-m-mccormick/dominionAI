04f18c1b40b0817cd7ef74dfcd2f79bedba66830
Tim McCormick <tim.mccormick.osu@gmail.com>
2017-12-03 22:31:12 -0500

    DominionData

--- DominionData.py
+++ DominionData.py
@@ -19,12 +19,12 @@ class DominionData:
     """
     Statistics package for evaluating dominion strategies
     """
-    def __init__(self,n_players=2, strategy=[BigMoney,BigMoneySmithy], cards=None, n_games = 1):
+    def __init__(self,num_players=2, strategy=[BigMoney,BigMoneySmithy], cards=None, n_games = 1):
         """constructor for DominionData"""
         self.verbose = False
-        self.n_players = n_players
+        self.n_players = num_players
         self.strats = strategy
-        if len(self.strats) != n_players:
+        if len(self.strats) != self.n_players:
             print('Need a number of strategies equal to number of players!')
             
         self.n_games = n_games
@@ -42,44 +42,44 @@ class DominionData:
                           'Workshop']
         
         #we need a default game to call to kingdom
-        self.def_game  = Game(n_players=2, strategy=self.strats, cards=self.cards, verbose=False)
-        self.kingdom   = Kingdom(self.def_game)
-        self.players   = [Player(self, strategy[n]) for n in range(n_players)]
+        #self.def_game  = Game(n_players=2, strategy=self.strats, cards=self.cards, verbose=False)
+        #self.kingdom   = Kingdom(self.def_game)
+        #self.players   = [Player(self, self.strats[n]) for n in range(self.n_players)]
         
         #Some individual quantities
-        self.ind_scores = []
-        self.ind_turns  = []
+        self.ind_scores = np.ndarray(shape=(self.n_games,self.n_players), dtype = int)
+        self.ind_turns  = np.ndarray(shape=(self.n_games), dtype = int)
         
         #Some average quantities
-        self.avg_scores = n_players*[0]
+        self.avg_scores = self.n_players*[0]
         
         #Some statistical quantities
         self.turn_std_dev   = None
-        self.scores_std_dev = n_players*[0]
+        self.scores_std_dev = self.n_players*[0]
         
         return None
     
     def run_simulation(self):
         """runs simulation and calculates some basic statistics on scores and turns"""
         for i in range(self.n_games):
-            game = Game(n_players=2, strategy=self.strats, cards=self.cards, verbose=False)
+            
+            game = Game(n_players=self.n_players, strategy=self.strats, cards=self.cards, verbose=False)
             game.play()
-            self.ind_scores[i] = += game.get_final_scores()
-            self.ind_turns[i]  = +=game.get_final_turn()
+            
+            self.ind_scores[i] = game.get_final_scores()
+            self.ind_turns[i]  = game.get_final_turn()
             
             self.avg_scores += game.get_final_scores()
             
             
         return None
     
-    #needs work
     def hist_scores(self):
         """Plots histogram of scores of each player"""
         hists = self.n_players*[0]
         bin_edges = self.n_players*[0]
         scoresTrans = self.ind_scores.T
-        hists = [np.histogram(scoresTrans[i]) for i in range(self.n_players)]
-        print(hists)
-        plt.bar(bin_edges[:-1], hists, width = 1)
-        plt.xlim(min(bin_edges), max(bin_edges))
-        plt.show()  
+        #hists = [np.histogram(scoresTrans[i]) for i in range(n_players)]
+        #plt.bar(bin_edges[:-1], hists, width = 1)
+        #plt.xlim(min(bin_edges), max(bin_edges))
+        #plt.show()  
--- dataTest.py
+++ dataTest.py
@@ -9,11 +9,11 @@ from DominionData import *
 from Strategy import * 
 import numpy as np
 
-dataTest = DominionData(n_players = 2, strategy=[BigMoney,BigMoneySmithy],cards=None,n_games=4)
+dataTest = DominionData(num_players = 2, strategy=[BigMoney,BigMoneySmithy],cards=None,n_games=4)
 dataTest.run_simulation()
 
-dataTest.hist_scores()
+#dataTest.hist_scores()
 #hist, bin = np.histogram(dataTest.ind_scores)
 
-print(dataTest.ind_scores)
+#print(dataTest.ind_scores)
 
